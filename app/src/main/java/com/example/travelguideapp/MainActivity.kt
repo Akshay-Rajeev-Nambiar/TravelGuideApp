5452d38005b1b322c80660d256ed3854edfe1d85
Akshey Rajeev Nambiar <akshayrajeevcr123@gmail.com>
2020-12-03 21:51:35 +0530

    new  new commit

--- app/src/main/java/com/example/travelguideapp/MainActivity.kt
+++ app/src/main/java/com/example/travelguideapp/MainActivity.kt
@@ -8,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //this is new message
         Toast.makeText(this, "this is new change", Toast.LENGTH_SHORT).show()
     }
 }
